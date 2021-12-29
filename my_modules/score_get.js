eb2cf244050bb400e058aff5ee7f0bb4c603a6d2
tmdeh <66948129+tmdeh@users.noreply.github.com>
2021-12-29 11:24:05 +0900

    'aaa'

--- my_modules/score_get.js
+++ my_modules/score_get.js
@@ -1,7 +1,7 @@
 const db = require('../db/sql');
 
 const get = async() => {
-    let sql = "SELECT nickname, score, time FROM score";
+    let sql = "SELECT nickname, score, time FROM score limit 10";
     return await db.executePreparedStatement(sql, []);
 }
 
